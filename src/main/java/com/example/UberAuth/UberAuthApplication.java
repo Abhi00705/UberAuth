c2cd530543534ba3867eae71457b465a06f073a6
Abhishek kumar <india52736628@gmail.com>
2026-03-05 11:26:54 +0530

    uberAuth 1st commit

---
+++ src/main/java/com/example/UberAuth/UberAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAuthApplication.class, args);
+	}
+
+}
